b54e5e0780898a9815798dfa1c0f99f6ff1c7d42
AlexanderSchaef <112415716+AlexanderSchaef@users.noreply.github.com>
2023-03-01 18:16:42 -0500

    Fixed link refs.

--- protected/index.php
+++ protected/index.php
@@ -5,11 +5,11 @@ include 'top.php'
 <main>
     <h2> index page </h2>
 
-    <img alt = "Josh" src="../images/Josh-Winter.png" alt="picture of Josh">
+    <img alt = "Josh" src="../images/josh-winter.png" alt="picture of Josh">
 
-    <img alt = "Alexander" src="../images/Alexander-Schaefer.png" alt="picture of Alexander">
+    <img alt = "Alexander" src="../images/Alexander-Schaefer.jpg" alt="picture of Alexander">
 
-    <img alt = "Abigail" src="../images/abi.jpeg" alt="Picture of Abigail">
+    <img alt = "Abigail" src="../images/abi.jpg" alt="Picture of Abigail">
 </main>
 
 <?php
