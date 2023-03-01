f5881b2ebaafb67ec6744c84278f13d618036900
AlexanderSchaef <112415716+AlexanderSchaef@users.noreply.github.com>
2023-03-01 18:13:54 -0500

    Update index.php

--- protected/index.php
+++ protected/index.php
@@ -5,11 +5,11 @@ include 'top.php'
 <main>
     <h2> index page </h2>
 
-    <img alt = "Josh" src="Josh-Winter.png" alt="picture of Josh">
+    <img alt = "Josh" src="../images/Josh-Winter.png" alt="picture of Josh">
 
-    <img alt = "Alexander" src="Alexander-Schaefer.png" alt="picture of Alexander">
+    <img alt = "Alexander" src="../images/Alexander-Schaefer.png" alt="picture of Alexander">
 
-    <img alt = "Abigail" src="abi.jpeg" alt="Picture of Abigail">
+    <img alt = "Abigail" src="../images/abi.jpeg" alt="Picture of Abigail">
 </main>
 
 <?php
